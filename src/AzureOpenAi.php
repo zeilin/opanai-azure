cc89283479a2ca0c589f012444c84850a504dd6e
zed.zhao <954489346zyz@sina.com>
2023-08-13 15:18:49 +0800

    feat: 接口超时时间改成300s

--- src/AzureOpenAi.php
+++ src/AzureOpenAi.php
@@ -61,7 +61,7 @@ class AzureOpenAi
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
+                CURLOPT_TIMEOUT => 300,
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => $method,
